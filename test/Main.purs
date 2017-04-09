6acad26e21a65d093881de81548c91b7d0a1df53
Marcin Szamotulski <mszamot@gmail.com>
2017-04-09 18:24:17 +0100

    Add test for mapProps

--- test/Main.purs
+++ test/Main.purs
@@ -1,25 +1,25 @@
 module Test.Main where
 
 import Prelude
-
+import Recompose
 import Enzyme.ReactWrapper as E
 import React.DOM as R
 import React.DOM.Props as RP
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (log)
-import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
-import Data.Array as A
-import Data.Foreign (Foreign, toForeign)
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Except (runExcept)
+import Data.Either (Either(..))
+import Data.Foreign (F, Foreign, readString, toForeign)
+import Data.Foreign.Index ((!))
 import Data.Newtype (class Newtype, unwrap)
 import Enzyme.Mount (mount)
 import React (ReactClass, createClass, createElement, getChildren, getProps, spec)
-import Test.Unit (success, suite, test, timeout)
+import Recompose.Class (class WithContextProps)
+import Test.Unit (failure, suite, test)
 import Test.Unit.Assert (assert, equal)
 import Test.Unit.Karma (runKarma)
 import Unsafe.Coerce (unsafeCoerce)
-
-import Recompose
-import Recompose.Class (class WithContextProps)
 
 newtype ButtonProps = ButtonProps { color :: String }
 
@@ -61,6 +61,7 @@ messageList = createClass $ (spec unit renderFn) { displayName = "MessageList" }
 messageListWithContext :: ReactClass { messages :: Array String }
 messageListWithContext = withContext messageList "#a0a0a0"
 
+main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR | eff) Unit
 main = runKarma do
   suite "context" do
     test "withContext and getContext"
@@ -74,3 +75,32 @@ main = runKarma do
       in do
         let color = _.color <<< coerceProps $ btnProps
         assert ("wrong context has been passed: " <> color) (color == "#a0a0a0")
+
+  suite "mapProps" do
+    test "mapProps" 
+      let
+        helloCls :: ReactClass { text :: String }
+        helloCls = createClass $ (spec unit renderFn) { displayName = "HelloCls" }
+
+        renderFn this = do
+          text <- getProps this >>= (pure <<< _.text)
+          pure $ R.div [ RP.className "msg", RP._data { msg: text } ] [ R.text text ]
+
+        f :: { msg :: String } -> { text :: String }
+        f text = { text: text.msg }
+
+        cls :: ReactClass { msg :: String }
+        cls = setDisplayName "HelloClsMapped" $ mapProps f helloCls
+
+        wrapper = E.find (mount (createElement cls { msg: "Hello World!" } []) (toForeign {})) ".msg"
+
+        readProps :: Foreign -> F { msg :: String }
+        readProps value = do
+          msg <- value ! "data-msg" >>= readString
+          pure { msg }
+      in 
+        case runExcept $ readProps $ E.props wrapper of
+          Left _ -> failure "ups..."
+          Right props -> do
+            let text = props.msg
+            assert ("wrong text prop " <> text) $ text == "Hello World!"
