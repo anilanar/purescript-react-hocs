c287a24b4ac4f17a568ec969a0bcb46d281395bd
Marcin Szamotulski <mszamot@gmail.com>
2017-04-13 01:20:22 +0100

    deferByAnimationFrame HOC

---
+++ src/Recompose/Defer.js
@@ -0,0 +1,5 @@
+"use strict"
+
+exports.nullElement = null
+
+exports.requestAnimationFrame = window.requestAnimationFrame
---
+++ src/Recompose/Defer.purs
@@ -0,0 +1,46 @@
+module Recompose.Defer 
+  ( deferByAnimationFrame
+  ) where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import React as R
+import React (ReactClass, ReactSpec, ReactElement)
+
+import Recompose.DisplayName (getDisplayName)
+
+foreign import nullElement :: ReactElement
+
+foreign import requestAnimationFrame :: forall eff. Eff eff Unit -> Eff eff Unit
+
+-- | Insipred by [DeferRender](https://gist.github.com/sieverk/b8815bf0b64e1fc0ef0aac298b7298f4#file-defercomponentrender-js)
+-- | Useful for rendering heavy components to allow the browser finish
+-- | animation.  This might help with percieving time of rendering.  Check the original
+-- | [blog post](https://medium.com/@paularmstrong/twitter-lite-and-high-performance-react-progressive-web-apps-at-scale-d28a00e780a3#983c).
+deferByAnimationFrame
+  :: forall eff props
+  .  Int
+  -> ReactClass props
+  -> ReactSpec props { count :: Int } eff
+deferByAnimationFrame count cls = (R.spec { count } renderFn)
+    { displayName = getDisplayName cls <> "Defer"
+    , componentDidMount = componentDidMount
+    }
+  where
+    componentDidMount this = do
+      requestAnimationFrame (frmEff this)
+
+    frmEff this = do
+      n <- R.readState this >>= (pure <<< _.count)
+      R.transformState this (_ { count = n - 1 })
+      if count > 0
+        then requestAnimationFrame (frmEff this)
+        else pure unit
+
+
+    renderFn this = do
+      props <- R.getProps this
+      n <- R.readState this >>= (pure <<< _.count)
+      if n > 0
+        then pure nullElement
+        else pure $ R.createElement cls props []
