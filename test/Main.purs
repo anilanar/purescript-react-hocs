089e09dbf1a0ce0cdad30ce04601818b3775e533
Marcin Szamotulski <mszamot@gmail.com>
2017-06-29 13:42:32 +0200

    add ref and readRef tests

--- test/Main.purs
+++ test/Main.purs
@@ -1,25 +1,32 @@
 module Test.Main where
 
-import Prelude
 import ReactHocs
-import ReactHocs.Class (class WithContextProps)
-import Enzyme.ReactWrapper as E
-import React.DOM as R
-import React.DOM.Props as RP
+
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
 import Control.Monad.Except (runExcept)
-import Data.Either (Either(..))
+import DOM (DOM)
+import DOM.HTML.Types (htmlElementToElement)
+import DOM.Node.Element (id) as Element
+import DOM.Node.Types (ElementId(..))
+import Data.Either (Either(Right, Left))
 import Data.Foreign (F, Foreign, readString, toForeign)
 import Data.Foreign.Index ((!))
 import Data.Newtype (class Newtype, unwrap)
-import Type.Proxy (Proxy(..))
 import Enzyme.Mount (mount)
+import Enzyme.ReactWrapper as E
+import Prelude (Unit, bind, discard, flip, pure, unit, ($), (<$>), (<<<), (<>), (==), (>>=))
 import React (ReactClass, createClass, createElement, getChildren, getProps, spec)
+import React.DOM as R
+import React.DOM.Props as RP
+import ReactHocs.Class (class WithContextProps)
 import Test.Unit (failure, suite, test)
-import Test.Unit.Assert (assert, equal)
+import Test.Unit.Assert (assert)
 import Test.Unit.Karma (runKarma)
+import Type.Proxy (Proxy(..))
 import Unsafe.Coerce (unsafeCoerce)
 
 newtype ButtonProps = ButtonProps { color :: String }
@@ -59,7 +66,7 @@ messageList = createClass $ (spec unit renderFn) { displayName = "MessageList" }
 messageListWithContext :: ReactClass { messages :: Array String }
 messageListWithContext = withContext messageList "#a0a0a0"
 
-main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR | eff) Unit
+main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR, dom :: DOM | eff) Unit
 main = runKarma do
   suite "context" do
     test "getContext"
@@ -127,3 +134,40 @@ main = runKarma do
           Right props -> do
             let text = props.msg
             assert ("wrong text prop " <> text) $ text == "Hello World!"
+
+  suite "refs" do
+    test "ref"
+      let
+        cls :: ReactClass Unit
+        cls = createClass $ (spec unit renderFn) { displayName = "Ref" }
+
+        renderFn this = do
+          pure $ R.div [ ref "div", RP._id "ref" ] []
+
+        wrp = mount $ createElement cls unit []
+
+        element = E.getDOMNode $ flip E.ref "div" $ wrp
+      in do
+        ElementId eid <- liftEff $ Element.id (htmlElementToElement element)
+        assert ("wrong element got: " <> eid <> "\n" <> E.debug wrp) $ eid == "ref"
+
+    test "readRef"
+      let
+        cls :: ReactClass Unit
+        cls = createClass $ (spec unit renderFn) { displayName = "Ref" }
+
+        renderFn this = do
+          pure $ R.div [ ref "div", RP._id "ref" ] []
+
+        wrp = mount $ createElement cls unit []
+
+        refs :: Foreign
+        refs = toForeign $ (unsafeCoerce (E.instance_ wrp)).refs
+
+      in do
+        fnode <- liftEff (unsafeCoerceEff $ readRef "div" refs)
+        case runExcept fnode of
+          Left _ -> failure ("reference div not found\n" <> E.debug wrp)
+          Right node -> do
+            ElementId eid <- liftEff $ Element.id (unsafeCoerce node)
+            assert ("wrong element got: " <> eid <> "\n" <> E.debug wrp) $ eid == "ref"
