afb2bcf2d2f2e42c24e2ca31b8db81aaf2b77291
Marcin Szamotulski <mszamot@gmail.com>
2017-07-21 11:37:19 +0200

    refFn, readRef: use HTMLElement

--- src/ReactHocs/Refs.purs
+++ src/ReactHocs/Refs.purs
@@ -7,7 +7,7 @@ module ReactHocs.Refs
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
-import DOM.Node.Types (Node, readNode)
+import DOM.HTML.Types (HTMLElement, readHTMLElement)
 import Data.Foreign (F, Foreign)
 import Data.Foreign.Index (readProp)
 import Prelude (Unit, join, pure, ($), (<$>))
@@ -17,13 +17,13 @@ import React.DOM.Props (Props, unsafeMkProps)
 ref :: String -> Props
 ref = unsafeMkProps "ref"
 
-readRef :: forall access eff. String -> Foreign ->  Eff (refs :: ReactRefs (read :: Read | access) | eff) (F Node) 
-readRef name refs = pure $ join $ readNode <$> readProp name refs
+readRef :: forall access eff. String -> Foreign ->  Eff (refs :: ReactRefs (read :: Read | access) | eff) (F HTMLElement) 
+readRef name refs = pure $ join $ readHTMLElement <$> readProp name refs
 
 -- | mutate `t` by overwritting a property with a new value
 foreign import unsafeSetProp :: forall e a p s. String -> ReactThis p s -> a -> Eff e Unit
 
 -- | example `refCb (unsafeSetProp "childElement" this)`
 -- | you can readit back from `this :: ReactThis props state` with `readRef name (toForeign this)`
-refFn :: forall access eff. (Node -> Eff (refs :: ReactRefs (write :: Write | access) | eff) Unit) -> Props
+refFn :: forall access eff. (HTMLElement -> Eff (refs :: ReactRefs (write :: Write | access) | eff) Unit) -> Props
 refFn fn = unsafeMkProps "ref" (\node -> unsafePerformEff (fn node))
