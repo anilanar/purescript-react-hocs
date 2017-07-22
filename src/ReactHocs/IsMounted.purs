08f0e7cacd07615f3a09f8fdd08c10aac767371c
Marcin Szamotulski <mszamot@gmail.com>
2017-07-22 22:50:11 +0200

    IsMounted: implement using ReactSpecIx

--- src/ReactHocs/IsMounted.purs
+++ src/ReactHocs/IsMounted.purs
@@ -1,34 +1,45 @@
 module ReactHocs.IsMounted 
-  ( readIsMounted
-  , writeIsMounted
+  ( isMountedIx
   , isMounted
+  , unsafeReadIsMounted
   ) where
 
+import Control.IxMonad ((:*>))
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, runEffFn1, runEffFn2)
-import Prelude (Unit, (*>))
+import Data.Symbol (SProxy(..))
 import React (ReactSpec, ReactThis)
+import React.Ix (ReactSpecIx, ReactThisIx(..), fromReactSpec, getProp, insertPropIx, setPropIx, toReactSpec)
+import React.Ix.EffR (EffR)
+import Unsafe.Coerce (unsafeCoerce)
 
-foreign import writeIsMountedImpl
-  :: forall props state e
-   . EffFn2 e (ReactThis props state) Boolean Unit
+unsafeReadIsMounted :: forall props state eff. ReactThis props state -> Eff eff Boolean
+unsafeReadIsMounted this = getProp (SProxy :: SProxy "isMounted__") (ReactThisIx this)
 
-writeIsMounted :: forall props state e. ReactThis props state -> Boolean -> Eff e Unit
-writeIsMounted this isMnt = runEffFn2 writeIsMountedImpl this isMnt
+isMountedIx
+  :: forall props state eff
+   . ReactSpec props state eff
+  -> ReactSpecIx props state (isMounted__ :: Boolean) (isMounted__ :: Boolean) (isMounted__ :: Boolean) eff
+isMountedIx s = f (fromReactSpec s)
+  where
+    sym :: SProxy "isMounted__"
+    sym = SProxy
 
-foreign import readIsMountedImpl
-  :: forall props state e
-   . EffFn1 e (ReactThis props state) Boolean
+    unsafeLift :: forall p s e. ReactSpecIx p s () () () e -> ReactSpecIx p s (isMounted__ :: Boolean) (isMounted__ :: Boolean) (isMounted__ :: Boolean) e
+    unsafeLift = unsafeCoerce
 
-readIsMounted :: forall props state e. ReactThis props state -> Eff e Boolean
-readIsMounted this = runEffFn1 readIsMountedImpl this
+    unsafeLift' :: forall a e. EffR e {} {isMounted__ :: Boolean} a -> EffR e {isMounted__ :: Boolean} {isMounted__ :: Boolean} a
+    unsafeLift' = unsafeCoerce
+
+    f spec
+      = let lSpec@{ componentWillMount, componentDidMount, componentWillUnmount } = (unsafeLift spec)
+        in lSpec
+          { componentWillMount = \this -> insertPropIx sym false this :*> unsafeCoerce (componentWillMount this)
+          , componentDidMount = \this -> setPropIx sym true this :*> componentDidMount this
+          , componentWillUnmount = \this -> componentWillUnmount this :*> setPropIx sym false this
+          }
 
 isMounted
   :: forall props state eff
    . ReactSpec props state eff
   -> ReactSpec props state eff
-isMounted spec@{ getInitialState, componentDidMount, componentWillUnmount } =
-  spec { getInitialState = \this -> writeIsMounted this false *> getInitialState this
-       , componentDidMount = \this -> writeIsMounted this true *> componentDidMount this
-       , componentWillUnmount = \this -> componentWillUnmount this *> writeIsMounted this false
-       }
+isMounted s = toReactSpec (isMountedIx s)
